f4a043b9bad8641c386e10ad0f991e56cf79c833
Marc <marc.yanez.perez@estudiantat.upc.edu>
2022-04-03 12:16:53 +0200

    Versión 2 generada por Marc

--- BBDD.sql
+++ BBDD.sql
@@ -8,8 +8,6 @@ CREATE TABLE Jugadores (
 	username TEXT NOT NULL,
 	IDj INTEGER PRIMARY KEY NOT NULL,
 	contra TEXT NOT NULL
-	
-
 )ENGINE=InnoDB;
 
 CREATE TABLE Partidas (
@@ -36,7 +34,6 @@ INSERT INTO Jugadores VALUES ('Juan',2,'juan1234');
 INSERT INTO Jugadores VALUES ('Marta',3,'marta1234');
 INSERT INTO Jugadores VALUES ('Maria',4,'maria1234');
 
-
 INSERT INTO Partidas VALUES ('Lunes',1,'Pedro');
 INSERT INTO Partidas VALUES ('Martes',2,'Marta');
 INSERT INTO Partidas VALUES ('Sabado',3,'Pedro');
@@ -46,20 +43,11 @@ INSERT INTO Participacion VALUES (1,1,1);
 INSERT INTO Participacion VALUES (2,3,3);
 INSERT INTO Participacion VALUES (3,2,2);
 INSERT INTO Participacion VALUES (1,2,3);
+
+
+
 INSERT INTO Participacion VALUES (2,2,4);
 INSERT INTO Participacion VALUES (4,3,2);
 INSERT INTO Participacion VALUES (3,3,4);
 INSERT INTO Participacion VALUES (1,3,1);
 INSERT INTO Participacion VALUES (2,4,3);
-
-
-
-
-
-
-
-
-
-
-
-
--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Monopoly
 
 Versión 1 generada por Luis
 Versión 1 verificada por Daniel
+Versión 1 comunicada po Marc	
+URL: https://youtu.be/NW8x_BjLg1o
+
+Versión 2 generada por Marc
